45d79ec85c8bb50bdd3aace72b46ee22844588be
LittleBall0fFur <naomi.roberta.ans.poort@gmail.com>
2016-11-24 11:27:47 +0100

    Torches implementation

---
+++ Torch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+    private GameObject enemy;
+    private GameObject player;
+    private GameObject torch;
+
+	// Use this for initialization
+	void Start () {
+        if (this.gameObject.name == "Torch1")
+        {
+            enemy = GameObject.Find("FireEnemy1");
+            torch = GameObject.Find("Torch1");
+        }
+        else if (this.gameObject.name == "Torch2")
+        {
+            enemy = GameObject.Find("FireEnemy2");
+            torch = GameObject.Find("Torch2");
+        }
+        player = GameObject.Find("Queen");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemy!= null && !enemy.GetComponent<TorchEnemy>().getHealthSystem().deathCheck())
+        {
+            torch.transform.position = enemy.transform.position;
+        }
+        else
+        {
+            torch.transform.position = player.transform.position;
+        }
+        
+	}
+}
---
+++ TorchTrigger.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TorchTrigger : MonoBehaviour {
+    private bool flag;
+    private GameObject p;
+    private GameObject gate;
+    private Vector3 position;
+    private GameObject torch;
+    private GameObject light;
+    private GameObject fence;
+    private float timer;
+    private bool notPlayed = true;
+    // Update is called once per frame
+    void Start()
+    {
+        if (this.gameObject.name == "FirstPillar")
+        {
+            p = GameObject.Find("ParticleSystem1");
+            p.GetComponent<Renderer>().enabled = false;
+            light = GameObject.Find("Light1");
+            light.SetActive(false);
+            gate = GameObject.Find("Fence1");
+            position = new Vector3(gate.transform.position.x, 1.66f, gate.transform.position.z);
+            torch = GameObject.Find("Torch1");
+            
+        }
+        else if (this.gameObject.name == "SecondPillar")
+        {
+            p = GameObject.Find("ParticleSystem2");
+            p.GetComponent<Renderer>().enabled = false;
+            light = GameObject.Find("Light2");
+            light.SetActive(false);
+            gate = GameObject.Find("Fence2");
+            position = new Vector3(gate.transform.position.x, 1.66f, gate.transform.position.z);
+            torch = GameObject.Find("Torch2");
+        }
+        
+    }
+
+	void Update () {
+        if (flag)
+        {
+           timer += Time.deltaTime;
+            p.GetComponent<Renderer>().enabled = true;
+            light.SetActive(true);
+            gate.transform.position = Vector3.MoveTowards(gate.transform.position, position, 0.15f);
+            AudioSource audio = gate.GetComponent<AudioSource>();
+            if (notPlayed)
+            {
+                audio.Play(1);
+                notPlayed = false;
+            }
+            if (timer >= 5) audio.Stop();
+            Destroy(torch);
+        }
+	}
+ 
+    public void OnTriggerEnter(Collider item)
+    {
+        if (item.gameObject.tag == "Player")
+        {
+            notPlayed = true;
+            flag = true;
+        }
+    }
+}
